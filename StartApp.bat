1a0a92ccd560b132dc2a4d6fe83fec402eec397a
pulcino <alfredo.pulcino@afp.com>
2018-08-03 17:32:03 +0200

    scripts et docs

--- StartApp.bat
+++ StartApp.bat
@@ -1,3 +1,4 @@
+start "apigateway" node c:/Users/apulcino/MSAFO/apigateway/server.js
 start "aforegistry" node c:/Users/apulcino/MSAFO/aforegistry/server.js
 start "afoevents" node c:/Users/apulcino/MSAFO/afoevents/server.js
 start "afopaniers" node c:/Users/apulcino/MSAFO/afopaniers/server.js
--- library/constantes.js
+++ library/constantes.js
@@ -1,18 +1,26 @@
 const os = require('os');
 const fetch = require('node-fetch');
-const MSRegistryUrl = 'http://localhost:5555/registry';
+const MSRegistryUrl = process.env.MS_REGISTRY_URL || 'http://localhost:5555/registry';
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 exports.MSTypeEnum = Object.freeze({
     "afoEvents": 1,
     "afoPaniers": 2,
     "afoAuthent": 3
 });
-
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+exports.MSPathnameEnum = Object.freeze({
+    "afoHealth": "/health",
+    "afoEvents": "/api/events",
+    "afoPaniers": "/api/selections",
+    "afoAuthent": "/api/user"
+});
 //------------------------------------------------------------------------------
 // http://localhost:5555/registry/declare/MSType?url=....
 //------------------------------------------------------------------------------
-exports.declareService = function (type, host, port) {
-    let MSurl = 'http://' + host + ':' + port;
-    const url = MSRegistryUrl + '/declare/' + type + '?url=' + MSurl;
+exports.declareService = function (type, host, port, pathname) {
+    const url = MSRegistryUrl + '/declare/' + type + '?host=' + host + '&port=' + port + '&pathname=' + pathname;
     console.log('GET : ', url);
     return new Promise(function (resolve, reject) {
         fetch(url, {
@@ -26,7 +34,27 @@ exports.declareService = function (type, host, port) {
         });
     });
 };
-
+//------------------------------------------------------------------------------
+// http://localhost:5555/registry/list
+// [{"type":"3","url":"http://158.50.163.7:3000","pathname":"/api/user","status":true,"cptr":331}]
+//------------------------------------------------------------------------------
+exports.getServiceList = function () {
+    const url = MSRegistryUrl + '/list';
+    console.log('GET : ', url);
+    return new Promise(function (resolve, reject) {
+        fetch(url, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        }).then(response => {
+            return response.json();
+        }).then(function (json) {
+            resolve(json);
+        }).catch(err => {
+            console.log('declareService : Error : ', err.message);
+            resolve([]);
+        });
+    });
+};
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 exports.getServerIpAddress = function () {
@@ -41,4 +69,19 @@ exports.getServerIpAddress = function () {
         }
     }
     return '';
+};
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+exports.findActiveMService = function (MServiceList, reqUrl) {
+    MServiceList = MServiceList || [];
+    reqUrl = reqUrl || '';
+    var resMService = MServiceList.find((value, index, array) => {
+        if (true === value.status) {
+            if (-1 !== reqUrl.indexOf(value.pathname)) {
+                return value;
+            }
+        }
+        return undefined;
+    });
+    return resMService;
 };
