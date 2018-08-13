96c6aeb2730beef46b38065006166573d3f75249
pulcino <alfredo.pulcino@afp.com>
2018-08-13 12:55:09 +0200

    Correction ajout git init

--- pullsrce.cmd
+++ pullsrce.cmd
@@ -1,7 +1,7 @@
 mkdir MSAFO && cd MSAFO && git init && git pull http://stid-vtfs2013.afp.local:8080/tfs/SICL/MSAFO/_git/root
-mkdir library && cd library && git pull http://stid-vtfs2013.afp.local:8080/tfs/SICL/MSAFO/_git/library
-cd .. && mkdir authent && cd authent && git pull http://stid-vtfs2013.afp.local:8080/tfs/SICL/MSAFO/_git/authent
-cd .. && mkdir afoevents && cd afoevents && git pull http://stid-vtfs2013.afp.local:8080/tfs/SICL/MSAFO/_git/afoevents
-cd .. && mkdir afopaniers && cd afopaniers && git pull http://stid-vtfs2013.afp.local:8080/tfs/SICL/MSAFO/_git/afopaniers
-cd .. && mkdir aforegistry && cd aforegistry && git pull http://stid-vtfs2013.afp.local:8080/tfs/SICL/MSAFO/_git/aforegistry
-cd .. && mkdir apigateway && cd apigateway && git pull http://stid-vtfs2013.afp.local:8080/tfs/SICL/MSAFO/_git/apigateway
+mkdir library && cd library && git init && git pull http://stid-vtfs2013.afp.local:8080/tfs/SICL/MSAFO/_git/library
+cd .. && mkdir authent && cd authent && git init && git pull http://stid-vtfs2013.afp.local:8080/tfs/SICL/MSAFO/_git/authent
+cd .. && mkdir afoevents && cd afoevents && git init && git pull http://stid-vtfs2013.afp.local:8080/tfs/SICL/MSAFO/_git/afoevents
+cd .. && mkdir afopaniers && cd afopaniers && git init && git pull http://stid-vtfs2013.afp.local:8080/tfs/SICL/MSAFO/_git/afopaniers
+cd .. && mkdir aforegistry && cd aforegistry && git init && git pull http://stid-vtfs2013.afp.local:8080/tfs/SICL/MSAFO/_git/aforegistry
+cd .. && mkdir apigateway && cd apigateway && git init && git pull http://stid-vtfs2013.afp.local:8080/tfs/SICL/MSAFO/_git/apigateway
